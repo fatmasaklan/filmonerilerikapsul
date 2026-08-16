629cb570273dc15991a327162730908ebfe73846
ahmetkaan42 <ahmetkaanedirneligil11@gmail.com>
2026-08-16 16:11:45 +0300

    ake

--- ai.engine.py
+++ ai.engine.py
@@ -85,5 +85,6 @@ def istatistikler():
 if __name__ == '__main__':
     print('📊 Film İstatistikleri:')
     print('reco')
+    print('ake')
     for stat in istatistikler():
         print(f'  {stat[0]:<15} | {stat[1]} film | Ort: ★ {stat[2]}')
